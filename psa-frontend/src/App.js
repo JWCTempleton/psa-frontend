9c6e93299f5d698336323270c0633e08ca51e34c
Jacob Templeton <jacob.templeton@gmail.com>
2023-05-09 15:35:45 -0700

    Add React router library, set up material ui baseline for styling

--- psa-frontend/src/App.js
+++ psa-frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Box sx={{ bgcolor: "white", height: "100vh" }}>
+          <p>Hello</p>
+        </Box>
+      </Container>
+    </React.Fragment>
   );
 }
-
-export default App;
