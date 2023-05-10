d6ba3d3026f7d482900c997dac0b07fc12f14741
Jacob Templeton <jacob.templeton@gmail.com>
2023-05-10 15:45:55 -0700

    Add connecting Login form to the login service, results in user successfully logging in when a valid username and password are submitted

--- psa-frontend/src/components/LoginForm.jsx
+++ psa-frontend/src/components/LoginForm.jsx
@@ -9,64 +9,108 @@ import {
   Link,
 } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { useNavigate } from "react-router-dom";
+import loginService from "../services/login";
+import cardService from "../services/cards";
 // import FormControlLabel from "@mui/material/FormControlLabel";
 // import Checkbox from "@mui/material/Checkbox";
-const LoginForm = () => {
+
+const LoginForm = ({
+  setUser,
+  setUsername,
+  setPassword,
+  username,
+  password,
+  user,
+}) => {
+  const navigate = useNavigate();
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      console.log("user", user);
+
+      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
+
+      cardService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+      navigate("/cards");
+    } catch (exception) {
+      // setErrorMessage("Wrong credentials");
+      // setTimeout(() => {
+      //   setErrorMessage(null);
+      // }, 5000);
+      console.log("error");
+    }
+  };
   const paperStyle = {
     padding: 20,
     height: "70vh",
     width: 400,
     margin: "50px auto",
   };
-  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const avatarStyle = { backgroundColor: "#42a5f5" };
   const btnstyle = { margin: "8px 0 16px 0" };
   const textField = { marginBottom: "10px" };
   return (
     <Grid>
-      <Paper elevation={10} style={paperStyle}>
-        <Grid align="center">
-          <Avatar style={avatarStyle}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <h2>Sign In</h2>
-        </Grid>
-        <TextField
-          label="Username"
-          placeholder="Enter username"
-          variant="outlined"
-          style={textField}
-          fullWidth
-          required
-        />
-        <TextField
-          label="Password"
-          placeholder="Enter password"
-          type="password"
-          variant="outlined"
-          fullWidth
-          required
-        />
-        {/* <FormControlLabel
+      <form onSubmit={handleLogin}>
+        <Paper elevation={10} style={paperStyle}>
+          <Grid align="center">
+            <Avatar style={avatarStyle}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <h2>Sign In</h2>
+          </Grid>
+          <TextField
+            label="Username"
+            placeholder="Enter username"
+            variant="outlined"
+            style={textField}
+            fullWidth
+            required
+            onChange={({ target }) => setUsername(target.value)}
+          />
+          <TextField
+            label="Password"
+            placeholder="Enter password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            required
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          {/* <FormControlLabel
           control={<Checkbox name="checkedB" color="primary" />}
           label="Remember me"
         /> */}
-        <Button
-          type="submit"
-          color="primary"
-          variant="contained"
-          style={btnstyle}
-          fullWidth
-        >
-          Sign in
-        </Button>
-        {/* <Typography>
+          <Button
+            type="submit"
+            color="primary"
+            variant="contained"
+            style={btnstyle}
+            fullWidth
+          >
+            Sign in
+          </Button>
+          {/* <Typography>
           <Link href="#">Forgot password ?</Link>
         </Typography> */}
-        <Typography>
-          {" "}
-          Do you have an account ?<Link href="#">Sign Up</Link>
-        </Typography>
-      </Paper>
+          <Typography>
+            {" "}
+            Do you have an account ?
+            <Link sx={{ m: 2 }} href="#">
+              Sign Up
+            </Link>
+          </Typography>
+        </Paper>
+      </form>
     </Grid>
   );
 };
--- psa-frontend/src/services/cards.js
+++ psa-frontend/src/services/cards.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 const baseUrl = "/api/cards";
 
-// let token = null;
+let token = null;
 
-// const setToken = (newToken) => {
-//   token = `Bearer ${newToken}`;
-// };
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
 
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
 };
 
-export default { getAll };
+export default { getAll, setToken };
