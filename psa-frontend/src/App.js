141bfb1d7563646cda8333c1364e7bd769e42eab
Jacob Templeton <jacob.templeton@gmail.com>
2023-05-09 17:02:19 -0700

    Add react router links, add basic pages that will be linked to, add Footer component

--- psa-frontend/src/App.js
+++ psa-frontend/src/App.js
@@ -1,15 +1,53 @@
 import * as React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Cards from "./components/Cards";
+import Users from "./components/Users";
+import Footer from "./components/Footer";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 
 export default function App() {
+  const padding = {
+    padding: 10,
+    fontWeight: 700,
+    textTransform: "uppercase",
+  };
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="md">
         <Box sx={{ bgcolor: "white", height: "100vh" }}>
-          <p>Hello</p>
+          <Router>
+            <div>
+              <Link style={padding} color="primary" variant="body2" to="/">
+                home
+              </Link>
+              <Link
+                style={padding}
+                color="primary"
+                variant="body2"
+                to="/cards  "
+              >
+                cards
+              </Link>
+              <Link style={padding} color="primary" variant="body2" to="/users">
+                users
+              </Link>
+            </div>
+
+            <Routes>
+              <Route path="/cards" element={<Cards />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+
+            <div>
+              <Footer />
+            </div>
+          </Router>{" "}
         </Box>
       </Container>
     </React.Fragment>
---
+++ psa-frontend/src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+
+export default function GuestFooter() {
+  return (
+    <Paper
+      sx={{ marginTop: "calc(10% + 60px)", bottom: 0 }}
+      component="footer"
+      square
+      variant="outlined"
+      elevation={3}
+    >
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: "flex",
+            my: 1,
+          }}
+        ></Box>
+
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: "flex",
+            mb: 2,
+          }}
+        >
+          <Typography variant="caption" color="initial">
+            2023 JWCTempleton Designs
+          </Typography>
+        </Box>
+      </Container>
+    </Paper>
+  );
+}
